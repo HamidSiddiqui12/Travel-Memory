508fe2c8a4de8d65b6432182749cf196b7bcc21a
Cloud User <ec2-user@ip-10-0-1-243.ap-south-1.compute.internal>
2026-07-14 05:36:30 +0000

    Update application code for AWS deployment

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,19 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const conn = require('./conn')
 app.use(express.json())
 app.use(cors())
 
 const tripRoutes = require('./routes/trip.routes')
 
-app.use('/trip', tripRoutes) // http://localhost:3001/trip --> POST/GET/GET by ID
+app.use('/api/trips', tripRoutes) // http://localhost:3001/trip --> POST/GET/GET by ID
 
 app.get('/hello', (req,res)=>{
     res.send('Hello World!')
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, "0.0.0.0", ()=>{
     console.log(`Server started at http://localhost:${PORT}`)
 })
